65a2180810016fd51c964a96c6c9846f63e6ce2f
avrians <avriansyahb10@gmail.com>
2024-08-30 22:13:39 +0700

    [av][add function proccessing]

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os,cv2
 
 app = Flask(__name__)
 
@@ -8,3 +9,12 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Fungsi untuk memproses gambar deteksi
+def processimg(img):
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (size, size)) 
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    equalized_img = clahe.apply(img)
+    crop=cv2.resize(equalized_img,(size,size))
+    return crop
